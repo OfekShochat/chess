482f127719c9d99670ea0521bc102d177ac8d6b5
ghostway <50481015+OfekShochat@users.noreply.github.com>
2022-07-01 22:06:07 +0300

    startfen

--- src/lib.zig
+++ src/lib.zig
@@ -7,3 +7,5 @@ pub const Square = @import("square.zig").Square;
 pub const Color = @import("color.zig").Color;
 
 pub const Piece = @import("piece.zig").Piece;
+
+pub const startfen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
